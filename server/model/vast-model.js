8f0b36e53f35a77ca3f6d308bcf6a84d3c6eac5f
segalzeyalz <segaleseo@gmail.com>
2019-01-23 21:42:29 +0200

    make the add vast req

--- server/model/vast-model.js
+++ server/model/vast-model.js
@@ -9,7 +9,20 @@ function createVast (vastURL, position, hideUI){
     //Set deafult values
     position = position || `bottom_right`;
     hideUI = hideUI? 1:0;
-}
+    db.query(`insert into vasts (vastURL, position, hideUI) values (${vastURL}, ${position}, ${hideUI})`,(error,rows,fields)=>{
+        if(error) {
+            dbFunc.connectionRelease;
+            reject(error);
+        } else {
+            dbFunc.connectionRelease;
+            resolve(rows);
+        }
+   });
+};
 
+function getVastXML(id){
+    //Should return XML according to id
+    return id;
+}
 
 module.exports = vastModel;
