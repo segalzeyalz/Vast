adb5d8365efc8d45ce28d92252b6bbfc5d1162f3
segalzeyalz <segaleseo@gmail.com>
2019-01-23 20:37:52 +0200

    use a modular structure

---
+++ server/config/api-config.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var api = require('../routes/api');
+var path  = require('path');
+var db = require('./database');
+
+var port = process.env.PORT || 3000;
+
+app.use('/api',api)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next)=> {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
