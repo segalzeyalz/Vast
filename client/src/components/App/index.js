1329da65f4dd624866acacfb72f8fd5317cdd409
segalzeyalz <segaleseo@gmail.com>
2019-01-24 02:13:49 +0200

    make client structure

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import CSS from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={CSS.App}>
       </div>
     );
   }
---
+++ client/src/constants/actionType.js
@@ -0,0 +1,1 @@
+export const ADD_VAST = 'ADD_VAST';
