29a84261a3558407c00186f80d7fb6955900cab6
segalzeyalz <segaleseo@gmail.com>
2019-01-26 19:20:05 +0200

    adding a menu

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import View from './../../containers/View'
-import ViewId from './../../containers/View/ViewId'
-import Form from './../../containers/Form'
-import Header from './../Header'
+import View from './../../containers/View';
+import ViewId from './../../containers/View/ViewId';
+import Form from './../../containers/Form';
+import Header from './../Header';
+import Button from '@material-ui/core/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+
 import CSS from './App.css';
 
+library.add(faIgloo)
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className={CSS.App}>
           <Header/>
-          <Link to="/Form">Form</Link>
+          <div className={CSS.Menu}>
+            <Link to="/Form"><Button  variant="contained" color="primary" >Form</Button></Link>
+            <Link to="/"><Button  variant="contained" color="primary" >Home</Button></Link>
+          </div>
           <Switch>
               <Route exact path="/" component={View}/>
               <Route path="/vast/:id" component={ViewId}/>
